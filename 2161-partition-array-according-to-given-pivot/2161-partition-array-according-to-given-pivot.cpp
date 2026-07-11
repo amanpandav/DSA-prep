eb6c22bfb313d9cd9e53f90bdd1b7556ce0adc04
amanpandav <amanpandav1222@gmail.com>
2026-07-11 22:00:04 +0530

    Time: 8 ms (49.7%), Space: 127.6 MB (88.35%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> result(nums.size());
-        int p = 0;
-        for(int i=0;i<nums.size();i++){
+        int less = 0;
+        int eql = 0; int more = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
             if(nums[i]<pivot){
-                result[p++] = nums[i];            
+                less++;
+            }
+            else if(nums[i]==pivot){
+                eql++;
+            }
+            else{
+                more++;
             }
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==pivot){
-                result[p++] = nums[i];            
+        int lessptr = 0;
+        int eqlptr = less;
+        int moreptr = less + eql;
+        vector<int> result(n);
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                result[lessptr++] = nums[i];
+            }
+            else if(nums[i]==pivot){
+                result[eqlptr++] = nums[i];
+            }
+            else{
+                result[moreptr++] = nums[i];
             }
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>pivot){
-                result[p++] = nums[i];            
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            nums[i] = result[i];
-        }
-        return nums;
+        return result;
     }
 };
